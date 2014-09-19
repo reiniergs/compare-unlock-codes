d37e565da5d080c2df1790e3d276323143d75f9b
unknown <rguerra@RGUERRA7010.revelex.com>
2014-09-19 16:17:16 -0400

    update

--- app.js
+++ app.js
@@ -1,59 +1,51 @@
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
+var routes = require('./routes');
 var http = require('http');
 var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var exphbs = require('express3-handlebars');
+var helpers = require('./lib/helpers.js');
 
-var routes = require('./routes');
-var users = require('./routes/user');
+
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(app.router);
-
-app.get('/', routes.index);
-app.get('/users', users.list);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+// config handlebars engine
+hbs = exphbs.create({
+    defaultLayout: 'main',
+    helpers: helpers,
+    partialsDir: 'views/partials/'
 });
 
-/// error handlers
+// Register `hbs` as our view engine using its bound `engine()` function.
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+app.get('/', routes.index);
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
-
-
-module.exports = app;
